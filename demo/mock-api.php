6bc8de3276b2dda4d294a5b00ec4941352c18acc
Aaron Stoddart <aaron@inkbeard.com>
2016-12-12 16:33:38 -0600

    Delete mock-api.php

--- demo/mock-api.php
+++
@@ -1,7 +0,0 @@
-<?php
-sleep(1);
-header('Cache-Control: no-cache, must-revalidate');
-header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
-header('Content-type: application/json');
-?>
-{"statusText": "OK", "status": "200"}
